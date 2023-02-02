ac8d4000e8fbbf4dc2c61e8269514d81a80da1b9
misawa2048 <misawa@elix.jp>
2023-02-03 03:03:40 +0900

    Update global.js

--- gamebook/global.js
+++ gamebook/global.js
@@ -1,5 +1,5 @@
 let API_POINT = "https://script.google.com/macros/s/";
 let DEF_SHEET_NAME = 'masterSheet';
 let MUTE_SE = "" //"./audio/mute.wav";
-var g_apiKey = "YOUR_DEPLOY_ID";
+var g_apiKey = "YOUR_DEPLOYMENT_ID";
 var g_isDebug = false;
